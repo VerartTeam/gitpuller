dd1428344bbd3c2d1af39431903bf4e480d4f9a5
Neylz <neylzvii@gmail.com>
2024-06-02 22:12:23 +0200

    init

---
+++ src/main/java/dev/neylz/gitpuller/GitPuller.java
@@ -0,0 +1,13 @@
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class GitPuller implements ModInitializer {
+    public static final String MOD_ID = "gitpuller";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Hello Fabric world!");
+    }
+}
